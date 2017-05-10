a525c9679246bfb73064b9c8d90a7e44219e3620
taozhaolib <tao.zhao.lib@gmail.com>
2017-05-10 11:14:12 -0500

    implemented authentication and authorization based on custom database

--- web/src/main/java/com/xprotocol/web/security/SecurityConfig.java
+++ web/src/main/java/com/xprotocol/web/security/SecurityConfig.java
@@ -1,39 +1,62 @@
-package com.xprotocol.web.config;
+package com.xprotocol.web.security;
 
+import javax.sql.DataSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.savedrequest.NullRequestCache;
 import org.springframework.session.web.http.HeaderHttpSessionStrategy;
 import org.springframework.session.web.http.HttpSessionStrategy;
 
+@Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    
+    @Autowired
+    private DataSource dataSource;
 
     @Bean
     public HttpSessionStrategy httpSessionStrategy() {
         return new HeaderHttpSessionStrategy();
     }
+    
+//    @Override
+//    public void configure(WebSecurity web) throws Exception {
+//        web.debug(true);
+//    }
 
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
         auth
-                .inMemoryAuthentication()
-                .withUser("sedooe").password("password").roles("USER");
+//                .inMemoryAuthentication()
+//                .withUser("sedooe").password("password").roles("USER");
+                    .jdbcAuthentication().dataSource(dataSource)
+                    .usersByUsernameQuery(" SELECT email AS username, password, userId FROM USERS WHERE email=? ")
+                    .authoritiesByUsernameQuery(" SELECT USERS.email AS username, ROLES.roleName AS role FROM USERS INNER JOIN USERROLES ON (USERS.userId=USERROLES.userId)"
+                            + "                                                                 INNER JOIN ROLES ON (USERROLES.roleId=ROLES.roleId) "
+                            + "                                                                 WHERE USERS.email=? ");
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
+                .antMatchers("/css/**","/images/**","/scripts/**").permitAll()
+                .antMatchers("/").permitAll()
+                .antMatchers("/login").permitAll()
+                .antMatchers("/registration").permitAll()
+                .antMatchers("/admin/**").hasAuthority("admin")
+                .antMatchers("/api/**").hasAuthority("admin")
                 .anyRequest().authenticated()
-                .and()
-                .requestCache()
-                .requestCache(new NullRequestCache())
-                .and()
-                .httpBasic();
+                .and().requestCache().requestCache(new NullRequestCache())
+                .and().httpBasic()
+                .and().formLogin().usernameParameter("email").passwordParameter("password").defaultSuccessUrl("/users")
+                .and().csrf().disable();
     }
 }
