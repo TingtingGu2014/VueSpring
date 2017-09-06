9c2ef46504650cf714061bde3e9108a331ceafe6
taozhaolib <tao.zhao.lib@gmail.com>
2017-09-06 15:19:44 -0500

    Added the vue-table-component example for tables

--- web/src/main/resources/static/src/UserList.vue
+++ web/src/main/resources/static/src/UserList.vue
@@ -1,11 +1,18 @@
 <template id="sign-up-template">
     
     <div class="container"><br><br>
-        <my-vuetable></my-vuetable>
+    <h3>List of Users</h3>
+        <VueDataTable 
+            :tableData = "tableData"
+            :tableColumns = "tableColumns"
+            :showFilter = "showFilter"
+        ></VueDataTable>
     </div>
 </template>
 
 <script>
+    
+    import VueDataTable from './VueDataTable.vue'
     
     var Utils = require('./Utils')
     
@@ -22,115 +29,36 @@
     export default {
         data: function() {
             return {
-                email: '',
-                alias: '',
-                password: '',
-                password2: '',
-                path: window.location.pathname,
+                tableData: [
+                    { firstName: 'John', birthday: '04/10/1940', songs: 72, lastName: 'White', address: '234 North Blue task AVE' },
+                    { firstName: 'Paul', birthday: '18/06/1942', songs: 70, lastName: 'White', address: '234 North Blue task AVE' },
+                    { firstName: 'George', birthday: '25/02/1943', songs: 22, lastName: 'White', address: '234 North Blue task AVE' },
+                    { firstName: 'Ringo', birthday: '07/07/1940', songs: 2, lastName: 'White', address: '234 North Blue task AVE' },
+                ],
+                tableColumns: [
+                    {show: 'firstName', label: 'First Name', dataType: ''},
+                    {show: 'lastName', label: 'Last Name', dataType: ''},
+                    {show: 'songs', label: 'Songs', dataType: 'numeric'},
+                    {show: 'birthday', label: 'Birthday', dataType: ''},
+                    {show: 'address', label: 'Address', },
+                ],
+                showFilter: true,
             }
         },
         computed: {
-            ...mapGetters({
-                isUserDetailsFetched: 'userModule/isUserDetailsFetched',
-                getUserDetails: 'userModule/getUserDetails',
-            })
+            
         },
         components: {
-            VuePassword,
+            VueDataTable,
         },
         watch: {
-            email: function(value) {
-                this.validate_email(value, 'email_message')
-            },
-            '$route' (to, from) {
-                this.path = to.meta.signUpType
-            },
+
         },
         methods: {
-            validate_email (value, email_message) {
-                if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(value)) {
-                    this[email_message] = '';
-                    return true;
-                } else {
-                    this[email_message] = 'Keep typing...waiting for a valid email';
-                    return false;
-                }
-            },
-            signupsubmit: function (message, event) {
-            
-                if(this.path.indexOf('signup') >= 0){
-                    if(Utils.isEmpty(this.email) || Utils.isEmpty(this.password) || Utils.isEmpty(this.password2)){
-                        alert("Please fill your complete information before registration.");
-                        return;
-                    }
-                    if(this.password != this.password2){
-                        alert("The two passwords are different.");
-                        return;
-                    }
-                }
-                else{
-                    if(Utils.isEmpty(this.email) || Utils.isEmpty(this.password)){
-                        alert("Please fill your complete information before registration.");
-                        return;
-                    }
-                }
-                
-                if (event){
-                    event.preventDefault()
-                }
-                
-                var qs = require('qs');
-                var url = '';
-                if(this.path.includes('signup')) {
-                    url = '/api/signUp'
-                    Utils.signUp(qs.stringify(this.$data), url)
-                    this.setDetailsFetched(false)
-                }
-                else {
-                    url = '/api/signIn'
-                }
-                axios({
-                    method: 'POST',
-                    url: url,
-                    dataType: 'json',
-                    headers: {
-                        'X-Requested-With': 'XMLHttpRequest',
-                    },
-                    auth: {
-                        username: this.email,
-                        password: this.password,
-                    },
-                })
-                .then( (response) => {
-                    var status = response.status;
-                    var data = response.data;
-
-                    if(status == 200){
-//                        alert("200");
-                        localStorage.userInfo = JSON.stringify(data)
-                        this.setDetailsFetched(false)
-                        document.location.href = '/home';
-                    }
-                    else{
-                        alert("Error status : " + status);
-                        return;
-                    }                                   
-                  })
-                  .catch( (error) => {
-                    console.log(error);
-                  });                                    
-            },
-            set_current_user: function() {
-                alert('new user!');
-            },
-            ...mapMutations({                
-                setUserDetails: 'userModule/setUserDetails',
-                setDetailsFetched: 'userModule/setDetailsFetched',
-            }),
+         
         }
     }
 </script>
 
-<style scoped>
-    
+
 </style>
--- web/src/main/resources/static/src/main.js
+++ web/src/main/resources/static/src/main.js
@@ -1,13 +1,14 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Vuex from 'vuex'
-import Vuetable from 'vuetable-2'
+import { TableComponent, TableColumn } from 'vue-table-component';
 import VuePassword from 'vue-password'
 
 Vue.use(VueRouter)
 Vue.use(Vuex)
-Vue.use(Vuetable)
 Vue.component(VuePassword)
+Vue.component('table-component', TableComponent);
+Vue.component('table-column', TableColumn);
  
 import App from './App.vue'
 import routejs from './Router.js'
