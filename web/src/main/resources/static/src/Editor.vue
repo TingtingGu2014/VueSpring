c18b5598f75348b8e6d1e30b1882ecf61ea88d89
taozhaolib <tao.zhao.lib@gmail.com>
2017-10-26 10:28:04 -0500

    initial checkin of editor component

---
+++ web/src/main/resources/static/src/Editor.vue
@@ -0,0 +1,56 @@
+<template id="editor-template">
+    <div class="container">
+        <form method="post">
+            <h3 style="font-weight: bold">{{heading}}</h3>
+            <textarea v-bind:id="editorName"></textarea>
+            <br>
+            <button type="button" class="btn btn-outline-success btn-lg">
+                <span style="font-weight: bold">Save</span>
+            </button>
+        </form>
+    </div>
+</template>     
+
+<script>
+            
+    export default {
+        
+        props: {
+            editorName: String,
+            heading: String,
+        },
+        data: function () {
+            return {
+
+            }
+        },    
+        mounted: function() {
+            tinymce.init({
+                selector: '#'+this.editorName,
+                plugins: [
+                    'advlist autolink link image imagetools lists charmap print preview hr anchor pagebreak spellchecker',
+                    'searchreplace wordcount visualblocks visualchars code fullscreen insertdatetime media nonbreaking',
+                    'save table contextmenu directionality emoticons template paste textcolor'
+                ],
+                toolbar: 'insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright ' +
+                         ' alignjustify | bullist numlist outdent indent | link image | print preview media fullpage ' +
+                         ' | forecolor backcolor emoticons | code ',
+                max_height: 500,
+                max_width: 500,
+                min_height: 300,
+                min_width: 400 
+            });
+        },
+        computed: {
+            
+        },
+        methods: {
+            
+        }
+    }
+
+</script>
+
+<style scoped>
+
+</style>
--- web/src/main/resources/static/src/Home.vue
+++ web/src/main/resources/static/src/Home.vue
@@ -1,15 +1,32 @@
 <template>
     <div>
         <br><h4>Check out the newly added protocols!</h4><br>
+        <Editor
+            :editorName = "editorName"
+            :heading = "heading"
+        >            
+        </Editor>
     </div>
 </template>
 
 <script>
+    import Editor from './Editor.vue'
+    
+    var Utils = require('./Utils')
+    var loggedIn = !Utils.isEmpty(Utils.readCookie('loggedIn'))
+    
+    
   export default {
     data: function(){
       return {
-        name: 'Home'
+        name: 'Home',
+        editorName: 'homeEditor',
+        heading: 'Create a new protocol',
+        loggedIn: loggedIn,
       }
+    },
+    components: {
+        Editor
     },
     methods: {
       alertName: function(){
